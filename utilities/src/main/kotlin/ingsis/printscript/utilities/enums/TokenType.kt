28f5db9ae6639b5c8c861462783d91f3b411e2e4
f-pochat <federico.pochat@ing.austral.edu.ar>
2023-03-28 16:22:54 -0300

    Unified tokens

--- utilities/src/main/kotlin/ingsis/printscript/utilities/enums/TokenType.kt
+++ utilities/src/main/kotlin/ingsis/printscript/utilities/enums/TokenType.kt
@@ -1,36 +1,42 @@
 package ingsis.printscript.utilities.enums
 
-enum class TokenType {
-    SEMICOLON,
-    COLON,
-    ASSIGNATION,
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_CURLY_BRACES,
-    RIGHT_CURLY_BRACES,
-    MINUS,
-    PLUS,
-    MULTIPLY,
-    DIVIDE,
-    GREATER_EQUAL,
-    GREATER,
-    LESS_EQUAL,
-    LESS,
-    IDENTIFIER,
-    STRING,
-    NUMBER,
-    PRINT,
-    LET,
-    CONST,
-    IF,
-    ELSE,
-    NUMBER_TYPE,
-    STRING_TYPE,
-    EOF,
+sealed interface Token
 
-    // Added for parser
-    TYPE, // STRING, NUMBER
-    FUNCTION, // FUNCTION VAL
-    LITERAL,
-    OPERATOR,
+sealed interface TokenType : Token
+
+object LET : TokenType
+object CONST : TokenType
+object IF : TokenType
+object ELSE : TokenType
+object COLON : TokenType
+object SEMICOLON : TokenType
+object ASSIGNATION : TokenType
+
+sealed interface Operation : Token
+
+object LEFT_PAREN : Operation
+object RIGHT_PAREN : Operation
+object ADD : Operation
+object SUB : Operation
+object DIV : Operation
+object MUL : Operation
+
+sealed interface Type : Token
+object NUM : Type
+object STR : Type
+
+sealed interface Value : Token
+class StrValue(val value: String) : Value {
+    override fun equals(other: Any?): Boolean {
+        return other is StrValue && value == other.value
+    }
 }
+
+class NumValue(val value: Double) : Value {
+    override fun equals(other: Any?): Boolean {
+        return other is NumValue && value == other.value
+    }
+}
+
+sealed interface Function : Token
+object PRINT : Function
