7a57064e31c6f0848d6b3d25035cbde958709a49
ignacio chalub <ignacio@ignacios-MacBook-Pro.local>
2023-03-30 19:17:59 -0300

    test commit

--- parser/src/main/kotlin/ingsis.printscript.parser/implementations/Parser.kt
+++ parser/src/main/kotlin/ingsis.printscript.parser/implementations/Parser.kt
@@ -1,7 +1,7 @@
 package ingsis.printscript.parser.implementations
 import ingsis.printscript.utilities.interfaces.IParser
 import ingsis.printscript.utilities.types.Token
-import ingsis.printscript.utilities.visitor.VisitableAST
+import ingsis.printscript.utilities.visitor.*
 
 class Parser : IParser {
     override fun parse(tokenList: List<Token>): VisitableAST {
